b1fd9bb5c79906571522272b85b4d5363eb10c8c
Hongfei Li <fever324@gmail.com>
2016-09-26 21:56:08 -0400

    Initial Commit

---
+++ ProfessorRatingsiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProfessorRatingsiOS
+//
+//  Created by Hongfei Li on 9/26/16.
+//  Copyright © 2016 Hongfei Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
