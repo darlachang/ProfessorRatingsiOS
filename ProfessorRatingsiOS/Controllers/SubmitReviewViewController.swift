7bbd7d6625a80152fd349ed6f7d6f3885b357bc1
Hongfei Li <fever324@gmail.com>
2016-11-09 14:10:23 -0500

    Fix param names for submit comment

--- ProfessorRatingsiOS/Controllers/SubmitReviewViewController.swift
+++ ProfessorRatingsiOS/Controllers/SubmitReviewViewController.swift
@@ -22,11 +22,11 @@ class SubmitReviewViewController: FormViewController {
         prof_id = "prof_id",
         course_id = "course_id",
         comment = "comment",
-        grading_difficulty = "grading_difficulty",
+        grading_difficulty = "grading",
         grade_received = "grade_received",
-        rating = "rating",
+        rating = "quality",
         tags = "tags",
-        work_load = "work_load",
+        work_load = "workload",
         show_major = "show_major",
         show_year = "show_year"
     }
